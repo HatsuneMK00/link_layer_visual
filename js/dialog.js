b92d3a5ed41476d6c12101c7a5125c18dc48750b
mikotokirino <shikirin556@gmail.com>
2019-02-27 02:03:43 +0800

    add question on page onFault and beginning

--- js/dialog.js
+++ js/dialog.js
@@ -12,5 +12,27 @@ $(document).ready(function () {
                 $(this).dialog("close");
             }
         }
+    });
+    $("#question").dialog({
+        dialogClass:"no-close",
+        autoOpen:false,
+        maxWidth:800,
+        maxHeight: 350,
+        width: 800,
+        height: 350,
+        modal: true,
+        buttons: {
+            "确定": function () {
+                if (document.getElementById("selection2").checked != true) {
+                    $(this).after("wrong")
+                }
+                else {
+                    $(this).dialog("close")
+                }
+            }
+        }
     })
 });
+function show_question() {
+    $("#question").dialog("open")
+}
